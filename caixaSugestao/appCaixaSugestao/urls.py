886e39e589ce6a5145be61314c223e334e5f188d
deodatojunior <deodatojunior999@gmail.com>
2020-10-10 21:26:53 -0400

    Adição de sugestao_remover, usuario_remover, modificações na views, url e htmls

--- caixaSugestao/appCaixaSugestao/urls.py
+++ caixaSugestao/appCaixaSugestao/urls.py
@@ -7,13 +7,17 @@ urlpatterns = [
     path('sugestao_form/', views.sugestao_novo, name='sugestao_novo'),
     path('sugestao_listar/', views.sugestao_listar, name='sugestao_listar'),
     path('sugestao_ver/<int:pk>', views.sugestao_ver, name='sugestao_ver'),
+    path('sugestao_remover/<int:pk>', views.sugestao_remover, name='sugestao_remover'),
     path('obrigado/', views.obrigado, name='obrigado'),
 
 
     path('login/', views.logar, name='login'),
+    path('deslogar/', views.deslogar, name='deslogar'),
+
 
     path('usuario_registrar/', views.usuario_registrar, name='usuario_registrar'),
-    path('usuario_listar/', views.usuario_listar, name='usuario_listar')
+    path('usuario_listar/', views.usuario_listar, name='usuario_listar'),
+    path('usuario_remover/<int:pk>', views.usuario_remover, name='usuario_remover')
 
 
 ]
--- caixaSugestao/appCaixaSugestao/views.py
+++ caixaSugestao/appCaixaSugestao/views.py
@@ -34,7 +34,11 @@ def obrigado(request):
 
 @login_required
 def sugestao_listar(request):
-    sugestao = Sugestao.objects.all()
+    query = request.GET.get("busca")
+    if query:
+        sugestao = Sugestao.objects.filter(texto__iexact=query)
+    else:
+        sugestao = Sugestao.objects.all()
     sugestoes = {'lista': sugestao}
     return render(request, 'appCaixaSugestao/sugestao_listar.html', sugestoes)
 @login_required
@@ -42,7 +46,13 @@ def sugestao_ver(request, pk):
     sugestao = get_object_or_404(Sugestao, pk=pk)
     return render(request, 'appCaixaSugestao/sugestao_ver.html', {'sugestao': sugestao})
 
-
+@login_required
+def sugestao_remover(request, pk):
+    sugestao = Sugestao.objects.get(pk=pk)
+    if request.method == "POST":
+        sugestao.delete()
+        return redirect('sugestao_listar')
+    return render(request, 'appCaixaSugestao/sugestao_remover.html', {'sugestao': sugestao})
 
 def logar(request):
     next = request.GET.get('next', '/usuario_listar/')
@@ -53,12 +63,16 @@ def logar(request):
         if user is not None:
             login(request, user)
             return HttpResponseRedirect(next)
-
         else:
             messages.error(request, 'Usuário ou senha incorretos.')
             return HttpResponseRedirect(settings.LOGIN_URL)
 
     return render(request, 'appCaixaSugestao/login.html', {'redirect_to': next})
+
+@login_required
+def deslogar(request):
+    logout(request)
+    return HttpResponseRedirect(settings.LOGIN_URL)
 
 @login_required
 def usuario_listar(request):
@@ -87,4 +101,23 @@ def usuario_registrar(request):
         messages.error(request, 'Permissão negada.')
         return redirect('/usuario_listar/')
 
-    return render(request, 'appCaixaSugestao/usuario_registrar.html', {})
+    return render(request, 'appCaixaSugestao/usuario_registrar.html')
+
+
+@login_required
+def usuario_remover(request, pk):
+    user = request.user
+    if user.has_perm('user.delete_user'):
+        try:
+            usuario = User.objects.get(pk = pk)
+            if request.method == "POST":
+                usuario.delete()
+                return redirect('usuario_listar')
+        except:
+            messages.error(request, 'Usuário não encontrado.')
+            return redirect('/usuario_listar/')
+    else:
+        messages.error(request, 'Permissão negada.')
+        return redirect('/usuario_listar/')
+
+    return render(request, 'appCaixaSugestao/usuario_remover.html', {'usuario': usuario})
